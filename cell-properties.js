d350a9f57e95f1ea248827fd28df6a5eacb8f979
Piyush Kumar <thakurpiyush16m@gmail.com>
2024-07-19 13:01:27 +0530

    iinitial Commit

---
+++ cell-properties.js
@@ -0,0 +1,171 @@
+//Storage
+let sheetDB = [];
+
+for(let i = 0; i < rows ; i++){
+ let sheetRow = [];
+for(let j = 0; j < cols ; j++){
+    let cellProp = {
+       bold : false,
+       italic : false,
+       underline : false,
+       alignment : "left",
+       fontFamily : "monospace",
+       fontSize : "14",
+       fontColor : "#000000",
+       bgColor : "#000000",
+    }
+    sheetRow.push(cellProp);
+}
+sheetDB.push(sheetRow);
+
+}
+
+//Selectors for cell properties
+
+let bold = document.querySelector('.bold');
+let italic = document.querySelector('.italic');
+let underline = document.querySelector('.underline');
+let fontSize = document.querySelector('.font-size-prop');
+let fontFamily = document.querySelector('.font-family-prop');
+let fontColor = document.querySelector('.font-color-prop');
+let bgColor = document.querySelector('.bg-color-prop');
+let alignment = document.querySelectorAll('.alignment');
+let leftAlign = alignment[0];
+let centerAlign = alignment[1];
+let rightAlign = alignment[2];
+
+//let addressBar = document.querySelector(".address-bar");  addressBar ko select krne ki jarurat nahi h wo phle se hi grid.js m declared h
+
+let activeColorProp = "#d1d8e0";
+let inactivecolorProp = "#ecf0f1";
+
+//Attach Property Listners
+
+bold.addEventListener("click" , (e) =>{
+    let address = addressBar.value;
+    let [cell , cellProp] =  activeCell(address);
+
+    //Modification
+    
+    cellProp.bold = !cellProp.bold; //Data change in object
+    cell.style.fontWeight = cellProp.bold ? "bold" : "normal";//UI change
+    bold.style.backgroundColor = cellProp.bold ? activeColorProp : inactivecolorProp ; //UI change
+})
+
+italic.addEventListener("click" , (e) =>{
+    let address = addressBar.value;
+    let [cell , cellProp] =  activeCell(address);
+
+    //Modification
+    
+    cellProp.italic = !cellProp.italic; //Data change in object
+    cell.style.fontStyle = cellProp.italic ? "italic" : "normal";//UI change
+    italic.style.backgroundColor = cellProp.italic ? activeColorProp : inactivecolorProp ; //UI change
+})
+
+underline.addEventListener("click" , (e) =>{
+    let address = addressBar.value;
+    let [cell , cellProp] =  activeCell(address);
+
+    //Modification
+    
+    cellProp.underline = !cellProp.underline; //Data change in object
+    cell.style.textDecoration = cellProp.underline ? "underline" : "none";//UI change
+    underline.style.backgroundColor = cellProp.underline ? activeColorProp : inactivecolorProp ; //UI change
+})
+
+fontSize.addEventListener("change" , (e) =>{
+    let address = addressBar.value;
+    let [cell , cellProp] =  activeCell(address);
+
+    cellProp.fontSize = fontSize.value; //Data change in object
+    cell.style.fontSize = cellProp.fontSize + "px";
+    fontSize.value = cellProp.fontSize;
+})
+
+fontFamily.addEventListener("change" , (e) =>{
+    let address = addressBar.value;
+    let [cell , cellProp] =  activeCell(address);
+
+    cellProp.fontFamily = fontFamily.value; //Data change in object
+    cell.style.fontFamily = cellProp.fontFamily 
+    fontFamily.value = cellProp.fontFamily;
+})
+
+fontColor.addEventListener("change" , (e) => {
+    let address = addressBar.value;
+    let [cell , cellProp] =  activeCell(address);
+
+    cellProp.fontColor = fontColor.value; //Data change in object
+    cell.style.color = cellProp.fontColor 
+    fontColor.value = cellProp.fontColor;
+
+})
+
+bgColor.addEventListener("change" , (e) => {
+    let address = addressBar.value;
+    let [cell , cellProp] =  activeCell(address);
+
+    cellProp.bgColor = bgColor.value; //Data change in object
+    cell.style.backgroundColor = cellProp.bgColor; 
+    bgColor.value = cellProp.bgColor;
+})
+
+alignment.forEach((alignElem) =>{
+    alignElem.addEventListener("click" , (e) => {
+        let address = addressBar.value;
+        let [cell , cellProp] =  activeCell(address);
+
+        let alignValue = e.target.classList[0];
+        cellProp.alignment = alignValue; //Data change in object
+        cell.style.textAlign = cellProp.alignment;
+
+        switch(alignValue){
+            case "left":
+             leftAlign.style.backgroundColor = activeColorProp;
+             centerAlign.style.backgroundColor = inactivecolorProp;
+             rightAlign.style.backgroundColor = inactivecolorProp;
+
+                break;
+
+                case "center":
+                    leftAlign.style.backgroundColor = inactivecolorProp;
+                    centerAlign.style.backgroundColor = activeColorProp;
+                    rightAlign.style.backgroundColor = inactivecolorProp;
+       
+                    break;
+
+                    case "right":
+                        leftAlign.style.backgroundColor = inactivecolorProp;
+                        centerAlign.style.backgroundColor = inactivecolorProp;
+                        rightAlign.style.backgroundColor = activeColorProp;
+           
+                        break;
+                
+        }
+
+        
+    })
+})
+
+
+
+
+
+
+
+function activeCell(address){
+   let [rid , cid] = decodeRidCidFromAddress(address);
+   //Acess cell & storage object
+   let cell = document.querySelector(`.cell[rid="${rid}"][cid="${cid}"]`);
+   let cellProp = sheetDB[rid][cid];
+   return [cell , cellProp];
+}
+
+function decodeRidCidFromAddress(address){
+    // address -> "A1"
+    let rid = Number(address.slice(1)-1);
+    let cid = Number(address.charCodeAt(0)-65);
+    return [rid , cid];
+
+}
---
+++ grid.js
@@ -0,0 +1,50 @@
+let rows = 100;
+let cols = 26;
+
+let addressColCont = document.querySelector(".address-column-cont")
+let addressRowCont = document.querySelector(".address-row-cont")
+let cellsCont = document.querySelector(".cells-cont")
+let addressBar = document.querySelector(".address-bar")
+for(let i = 0 ; i < rows ; i++){
+    let addressCol  = document.createElement("div");
+    addressCol.setAttribute("class" , "address-col");
+    addressCol.innerText = i+1;
+    addressColCont.appendChild(addressCol);
+}
+
+for(let i = 0 ; i < cols ; i++){
+    let addressRow = document.createElement("div");
+    addressRow.setAttribute("class" , "address-row");
+    addressRow.innerText = String.fromCharCode(65+i);//As the ascii value of "A =65" & "Z=90"
+    addressRowCont.appendChild(addressRow);
+}
+
+for(let i = 0 ; i < rows ; i++){ // This For loop is For creating 100 rows divs
+    let rowCont = document.createElement("div"); 
+    rowCont.setAttribute("class" , "row-cont");
+    for(let j = 0 ; j < cols ; j++){ // This for loop inside above for loop is creating 26 divs columnwise in a single row
+      let cell = document.createElement("div");
+      cell.setAttribute("class" , "cell");
+      cell.setAttribute("contenteditable", "true");//contenteditable="true": attribute in HTML Makes the element editable  directly on the webpage.
+      cell.setAttribute("spellcheck", "false");
+      //Attributs for cell and Storage identification
+      cell.setAttribute("rid", i); 
+      cell.setAttribute("cid", j);
+
+      rowCont.appendChild(cell);
+      addListnerForAddressBarDisplay(cell , i , j);
+    }
+    cellsCont.appendChild(rowCont);
+}
+
+function addListnerForAddressBarDisplay(cell, i , j){
+cell.addEventListener("click" , (e) =>{
+let rowId = i+1;
+let colId = String.fromCharCode(65+j);
+addressBar.value = `${colId}${rowId}`
+})
+}
+
+// By delfault we want the address bar to have some value that is first cell we dont want it to be empty for the edge cases
+let firstCell = document.querySelector(".cell");
+firstCell.click();//By default click ho jaega via DOM
